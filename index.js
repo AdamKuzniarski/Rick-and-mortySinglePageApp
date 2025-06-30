eb55d85e61e6c798e5f61d35b5dade6e89267a7c
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-06-30 14:24:11 +0200

    initial commit

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-// States
+// States 
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+
+// FetchAPI
