3fc0f07040548b5a8b0f31760a00d9e39c6aac50
Milia Laufenberg <coding.l@ufenberg.net>
2025-07-01 10:50:39 +0200

    searchQuery works with next/prev buttons

--- index.js
+++ index.js
@@ -6,55 +6,47 @@ export const cardContainer = document.querySelector(
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
-const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 const searchButton = document.querySelector('[data-js="search-button"]');
 const searchInput = document.querySelector('[data-js="search-input"]');
+
 // States
-const maxPage = 42;
+
+let maxPage = 1;
 let page = 1;
 let searchQuery = "";
 
 // FetchAPI
 
+
 async function fetchCharacters(page) {
   const response = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${page}`
+    `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`
   );
   const data = await response.json();
+  maxPage = data.info.pages;
+  pagination.textContent = `${page}/${maxPage}`;
   data.results.forEach((character) => {
     return cardContainer.append(createCharacterCard(character));
   });
 }
 
-async function fetchSearchCharacter(searchQuery) {
-  console.log(searchQuery);
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${"1"}&name=${searchQuery}`
-  );
-  const data = await response.json();
-  data.results.forEach((character) => {
-    return cardContainer.append(createCharacterCard(character));
-  });
-}
-
-fetchCharacters(page, searchQuery);
 // Eventlistener
 
 prevButton.addEventListener("click", () => {
-  cardContainer.innerHTML = "";
   if (page > 1) {
+    cardContainer.innerHTML = "";
     page--;
     fetchCharacters(page);
   }
   pagination.textContent = `${page}/${maxPage}`;
 });
+
 nextButton.addEventListener("click", () => {
-  cardContainer.innerHTML = "";
   if (page < maxPage) {
+    cardContainer.innerHTML = "";
     page++;
     fetchCharacters(page);
   }
@@ -69,5 +61,7 @@ searchButton.addEventListener("click", (event) => {
   cardContainer.innerHTML = "";
   page = 1;
   searchQuery = searchInput.value;
-  fetchSearchCharacter(searchQuery);
+  fetchCharacters(page, searchQuery);
 });
+
+fetchCharacters(page, searchQuery);
