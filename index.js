843a8b661e1326160cb143962563def6479979b3
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-01 10:30:54 +0200

    searchQuery changes

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
-
+const searchButton = document.querySelector('[data-js="search-button"]');
+const searchInput = document.querySelector('[data-js="search-input"]');
 // States
 const maxPage = 42;
 let page = 1;
-const searchQuery = "";
+let searchQuery = "";
 
 // FetchAPI
 
@@ -24,13 +25,23 @@ async function fetchCharacters(page) {
     `https://rickandmortyapi.com/api/character?page=${page}`
   );
   const data = await response.json();
-  //console.log(data);
   data.results.forEach((character) => {
     return cardContainer.append(createCharacterCard(character));
   });
 }
 
-fetchCharacters(page);
+async function fetchSearchCharacter(searchQuery) {
+  console.log(searchQuery);
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${"1"}&name=${searchQuery}`
+  );
+  const data = await response.json();
+  data.results.forEach((character) => {
+    return cardContainer.append(createCharacterCard(character));
+  });
+}
+
+fetchCharacters(page, searchQuery);
 // Eventlistener
 
 prevButton.addEventListener("click", () => {
@@ -48,4 +59,15 @@ nextButton.addEventListener("click", () => {
     fetchCharacters(page);
   }
   pagination.textContent = `${page}/${maxPage}`;
+});
+
+// Search Form
+
+searchButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  cardContainer.innerHTML = "";
+  page = 1;
+  searchQuery = searchInput.value;
+  fetchSearchCharacter(searchQuery);
 });
